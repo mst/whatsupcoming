fb1d4fb68e4990630152d75771a2d324f5d3dfa5
Marcel Steinbach <msteinba@gmail.com>
2012-04-06 17:07:26 +0200

    __unicode__ methods for models

--- eventsearch/models.py
+++ eventsearch/models.py
@@ -7,11 +7,17 @@ class Location(models.Model):
     zipCode = models.CharField(max_length="10")
     name = models.CharField(max_length="200")
 
+    def __unicode__(self):
+	return self.name
+
 class Event(models.Model):
     date_start = models.DateTimeField('start date')
     date_end = models.DateTimeField('end date')
     name = models.CharField(max_length="200")
     location = models.ForeignKey(Location)
-    
+
+    def __unicode__(self):
+	return self.name + " at " + self.location.__unicode__()
+
     class Meta:
         ordering = ["date_start"]
