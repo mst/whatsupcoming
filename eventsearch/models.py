31e5f4b0cfeb6d0be58ccd6e79a9d13598210ca1
Marcel <marcelst@me.com>
2012-05-05 16:14:37 +0200

    added distance calculation

--- eventsearch/models.py
+++ eventsearch/models.py
@@ -1,8 +1,9 @@
-
 import logging
 from decimal import *
 from django.db import models
 from googleplaces import GooglePlaces, types
+from string import Template
+import math
 
 YOUR_API_KEY = 'AIzaSyDsaRhBz8WhZICkiElokU9XitMWRkIFxL8'
 logging.basicConfig()
@@ -11,11 +12,32 @@ _logger.setLevel(logging.INFO)
 
 class Location(models.Model):
 
+    nauticalMilePerLat = 60.00721
+    nauticalMilePerLongitude = 60.10793
+    rad = math.pi / 180.0
+    milesPerNauticalMile = 1.15078
+    metersPerMile = 1609.344
+
     name = models.CharField(max_length="200")
     city = models.CharField(max_length="200", null=True)
     address = models.CharField(max_length="200",null=True)
     latitude = models.DecimalField(max_digits=30,decimal_places=26,blank=True)
     longitude = models.DecimalField(max_digits=30,decimal_places=26,blank=True)
+    
+    #
+    # The following formulas are adapted from the Aviation Formulary
+    # http://williams.best.vwh.net/avform.htm
+    #
+    def distance(self, lat2, lon2):
+        """
+        Caclulate distance between two lat lons in NM
+        """
+        lat1 = float(self.latitude)
+        lon1 = float(self.longitude)
+        yDistance = (lat2 - lat1) * self.nauticalMilePerLat
+        xDistance = (math.cos(lat1 * self.rad) + math.cos(lat2 * self.rad)) * (lon2 -lon1 ) * (self.nauticalMilePerLongitude / 2)
+        distance = math.sqrt( yDistance**2 + xDistance**2 )
+        return distance * self.milesPerNauticalMile * self.metersPerMile
     
     def save(self, *args, **kwargs):
 
@@ -54,5 +76,14 @@ class Event(models.Model):
     def __unicode__(self):
 	return self.name + " at " + self.location.__unicode__()
 
+    def distance(self, lat, lon):
+        return self.location.distance(lat, lon)
+
     class Meta:
         ordering = ["date_start"]
+
+    @classmethod
+    def find_closest(self, lat, lon, limit=25):
+        events = Event.objects.all()
+        return sorted(events, key=lambda event:event.distance(lat,lon))
+
--- eventsearch/views.py
+++ eventsearch/views.py
@@ -6,11 +6,12 @@ from datetime import datetime
 
 def upcoming(request):
     try:
-        events = Event.objects.filter(date_start__gt=datetime.now())
+        events = Event.objects.all() # filter(date_start__gt=datetime.now())
 	for event in events:
 	    event.remaining = event.date_start.replace(tzinfo=None) - datetime.utcnow().replace(tzinfo=None)
 	    event.days = event.remaining.days	    
 	    event.minutes = event.remaining.seconds/60
+            event.distance_view = round(event.distance(float(request.GET.get('lat')), float(request.GET.get('lon'))))
     except Event.DoesNotExist:
         raise Http404
     return render_to_response('eventsearch/upcoming.html', {'events': events})
