f1f7bae982b142fc80e71dc614b6aea17382cb05
Marcel <marcelst@me.com>
2012-05-06 19:37:57 +0200

    added admin info

--- whatsupcoming/settings.py
+++ whatsupcoming/settings.py
@@ -13,7 +13,8 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Marcel', 'marcelst@me.com'),
+    ('Uli', 'ulrich.koeller@gmail.com'),
 )
 
 MANAGERS = ADMINS
