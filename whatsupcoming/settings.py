f7603e0bf8c3091a7e491fd879f50aa606c0bea2
Uli Köller <uli.koeller@gmail.com>
2012-05-14 15:28:49 +0200

    #16 use humanize module for display time remaining

--- whatsupcoming/settings.py
+++ whatsupcoming/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'endless_pagination',
+    'django.contrib.humanize',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
